293ee74532e1ccf192fd8b7047410306e0ba5791
muqapro-sys <muqapro@gmail.com>
2026-06-01 03:43:29 +0300

    Move search controls above keyboard

--- app/src/main/java/com/khaly/videolibrary/MainActivity.kt
+++ app/src/main/java/com/khaly/videolibrary/MainActivity.kt
@@ -44,6 +44,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -272,6 +273,7 @@ fun VideoLibraryApp(viewModel: VideoLibraryViewModel) {
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .fillMaxSize()
+                    .imePadding()
                     .navigationBarsPadding()
                     .padding(bottom = 12.dp)
             ) {
